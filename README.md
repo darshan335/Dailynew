bffea10072bc3312b0d019ee8b9b9fa97efabed0
Darshan kg <darshan4106@gmail.com>
2025-08-06 21:21:48 +0530

    Day 1: Hello Java program

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Daily Code
+
+This repository contains my daily Java practice code organized by day folders.
---
+++ src/Day1/Main.java
@@ -0,0 +1,9 @@
+package Day1;
+
+// src/Day1/Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Day 1 - Hello, Java!");
+    }
+}
+
